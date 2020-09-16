d0e5ad6804b2920662ca2906c09e18740ea24dbf
DedeApenas <Andrelromao@hotmail.com>
2020-09-16 14:18:00 -0300

    Primeiros passos Python

---
+++ Aula002.py
@@ -0,0 +1,26 @@
+print("Hello")
+nome = "Dedé"
+print(nome)
+#Declaração de variaveis
+numeroInteiro = 2
+numeroDecimal = 2.45642
+palavraCaracter = "André"
+verdadeiroFalso = True
+#Mostrar na tela
+print(numeroInteiro)
+print(numeroDecimal)
+print(palavraCaracter)
+print(verdadeiroFalso)
+#Mostrar na tela o tipo de dado que a variavel armazena
+print(type(numeroInteiro))
+print(type(numeroDecimal))  
+print(type(palavraCaracter))
+print(type(verdadeiroFalso))
+#Recursos para usar com dados do tipo String
+print(len(palavraCaracter)) #Função "len" contar o numero de caracter
+print(palavraCaracter * 3) #Repetir valor da variavel "*"
+print(palavraCaracter + '', palavraCaracter) #Concatenar strings usar o "+",('' para dar espaço)
+#Recursos para usar com dados do tipo Float
+print(round(numeroDecimal,2)) #Arredondar "round" e numero de casas 
+print("%.2f" % numeroDecimal) #Outra forma de arredondamento e de definir numero de casas apos virgula
+
